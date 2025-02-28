c2c5e3409143b38bad1bf10bddf506ea68d42421
Jilliane Ferolino <158802020+jilyanf@users.noreply.github.com>
2025-02-28 10:28:07 +0800

    edited background color

--- app/(tabs)/editprofile.tsx
+++ app/(tabs)/editprofile.tsx
@@ -144,7 +144,7 @@ export default function EditProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.gold,
+    backgroundColor: Colors.bg,
     alignItems: 'center',
   },
   imageContainer: {
