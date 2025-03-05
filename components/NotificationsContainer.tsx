15f67ecbc8b025bcda1f380568754cef657d3f3c
Jilliane Ferolino <158802020+jilyanf@users.noreply.github.com>
2025-03-05 21:24:10 +0800

    clicking on notification will now show modal with full content

--- components/NotificationsContainer.tsx
+++ components/NotificationsContainer.tsx
@@ -1,7 +1,12 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { 
+  View, Text, StyleSheet, FlatList,
+  TouchableOpacity, Modal, TouchableWithoutFeedback
+  } from "react-native";
+import React, { useState } from "react";
 
 import { Fonts } from '@/constants/Fonts';
 import { Colors } from '@/constants/Colors';
+import { FontAwesome } from "@expo/vector-icons";
 
 interface Notification {
   id: string;
@@ -16,16 +21,67 @@ interface Props {
 }
 
 export default function NotificationsContainer({ notifications }: Props) {
+  const [selectedNotification, setSelectedNotification] = useState<Notification | null>(null);
+
+  const openNotificationModal = (notification: Notification) => {
+    setSelectedNotification(notification);
+  };
+
+  const closeNotificationModal = () => {
+    setSelectedNotification(null);
+  };
+
+  const renderNotificationModal = () => {
+    if (!selectedNotification) return null;
+
+    return (
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={!!selectedNotification}
+        onRequestClose={closeNotificationModal}
+      >
+        <TouchableWithoutFeedback onPress={closeNotificationModal}>
+          <View style={styles.modalOverlay}>
+            <TouchableWithoutFeedback>
+              <View style={styles.modalContainer}>
+                <View style={styles.modalHeader}>
+                  <Text style={styles.modalTitle}>{selectedNotification.title}</Text>
+                  <FontAwesome name="close" color={Colors.white} size={24} onPress={closeNotificationModal} />
+                </View>
+                
+                <View style={styles.modalContentContainer}>
+                  <Text style={styles.modalContent}>{selectedNotification.content}</Text>
+                </View>
+                
+                <View style={styles.modalDetailsContainer}>
+                  <Text style={styles.modalDate}>{selectedNotification.date}</Text>
+                  <View style={styles.modalDivider} />
+                  <Text style={styles.modalTime}>{selectedNotification.time}</Text>
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
         data={notifications}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <View style={styles.card}>
+          <TouchableOpacity 
+            style={styles.card}
+            onPress={() => openNotificationModal(item)}
+          >
             <View style={styles.leftSection}>
               <Text style={styles.title}>{item.title}</Text>
-              <Text style={styles.content}>{item.content}</Text>
+              <Text style={styles.content}  numberOfLines={2}>
+                {item.content}
+              </Text>
             </View>
 
             <View style={styles.divider} />
@@ -34,10 +90,12 @@ export default function NotificationsContainer({ notifications }: Props) {
               <Text style={styles.date}>{item.date}</Text>
               <Text style={styles.time}>{item.time}</Text>
             </View>
-          </View>
+          </TouchableOpacity>
         )}
         contentContainerStyle={styles.listContainer}
       />
+
+      {renderNotificationModal()}
     </View>
   );
 }
@@ -90,5 +148,62 @@ const styles = StyleSheet.create({
   time: {
     fontFamily: Fonts.regular,
     fontSize: 12,
+  },
+  // Modal Styles
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    width: '90%',
+    backgroundColor: Colors.white,
+    borderRadius: 15,
+    alignItems: 'center',
+    paddingBottom: 10,
+  },
+  modalHeader: {
+    width: '100%',
+    backgroundColor: Colors.gold,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  modalTitle: {
+    fontFamily: Fonts.semibold,
+    fontSize: 20,
+    color: Colors.white,
+  },
+  modalContentContainer: {
+    width: '100%',
+    marginVertical: 15,
+    paddingHorizontal: 20,
+  },
+  modalContent: {
+    fontFamily: Fonts.regular,
+    fontSize: 16,
+  },
+  modalDetailsContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    gap: 10,
+  },
+  modalDivider: {
+    width: 1,
+    height: '100%',
+    backgroundColor: Colors.border,
+  },
+  modalDate: {
+    fontFamily: Fonts.medium,
+    color: Colors.textSecondary,
+  },
+  modalTime: {
+    fontFamily: Fonts.medium,
+    color: Colors.textSecondary,
   },
 });
