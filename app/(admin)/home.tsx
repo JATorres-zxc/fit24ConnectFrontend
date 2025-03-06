c6425d290ec9e94194505eaf7e297a1f326e56eb
Jilliane Ferolino <158802020+jilyanf@users.noreply.github.com>
2025-03-06 13:17:35 +0800

    added dynamic announcements to admin side of homepage

--- app/(admin)/home.tsx
+++ app/(admin)/home.tsx
@@ -2,6 +2,9 @@ import { Text, View, StyleSheet } from "react-native";
 
 import Button from '@/components/CreateAnnouncementButton';
 import Header from '@/components/HomeHeader';
+import AdminAnnouncements from "@/components/AdminSideAnnouncementsContainer";
+
+import { announcements } from "@/constants/announcements";
 import { Colors } from "@/constants/Colors";
 
 export default function Home() {
@@ -11,6 +14,10 @@ export default function Home() {
 
       <View style={styles.buttonContainer}>
         <Button label="Create Announcement" theme="primary" />
+      </View>
+
+      <View style={styles.announcementsContainer}>
+        <AdminAnnouncements announcements={announcements} />
       </View>
 
     </View>
@@ -25,7 +32,12 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     width: '85%',
-    flex: 1,
     alignItems: 'center',
+    marginBottom: 10,
+  },
+  announcementsContainer: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
   },
 });
