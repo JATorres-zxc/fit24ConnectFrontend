5ed12dd83a627eb82989bd62ccb8fb86832c6cc2
Jilliane Ferolino <158802020+jilyanf@users.noreply.github.com>
2025-03-20 10:37:03 +0800

    added history screen to trainer side

---
+++ context/accessLogs.tsx
@@ -0,0 +1,11 @@
+export const accessLogs = [
+    { id: "1", facilityName: "Fit24 Fitness Gym", accessStatus: "Granted", date: "Mar 9, 2025", time: "07:10:21" },
+    { id: "2", facilityName: "Sauna", accessStatus: "Denied", date: "Mar 8, 2025", time: "22:13:41" },
+    { id: "3", facilityName: "Locker", accessStatus: "Granted", date: "Mar 5, 2025", time: "08:08:31" },
+    { id: "4", facilityName: "Locker", accessStatus: "Granted", date: "Mar 9, 2025", time: "01:24:21" },
+    { id: "5", facilityName: "Sauna", accessStatus: "Denied", date: "Mar 8, 2025", time: "04:53:11" },
+    { id: "6", facilityName: "Shower", accessStatus: "Granted", date: "Mar 5, 2025", time: "09:18:35" },
+    { id: "7", facilityName: "Locker", accessStatus: "Denied", date: "Mar 9, 2025", time: "20:50:25" },
+    { id: "8", facilityName: "Shower", accessStatus: "Granted", date: "Mar 8, 2025", time: "11:23:41" },
+    { id: "9", facilityName: "Lounge", accessStatus: "Denied", date: "Mar 5, 2025", time: "17:58:11" },
+  ];
