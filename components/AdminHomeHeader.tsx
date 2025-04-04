569c0651d19c87404a2fc383dc258e6dccb737b4
Jilliane Ferolino <158802020+jilyanf@users.noreply.github.com>
2025-04-04 11:20:17 +0800

    modified admin home header

---
+++ components/AdminHomeHeader.tsx
@@ -0,0 +1,47 @@
+import { router } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { Fonts } from '../constants/Fonts';
+import { Colors } from '@/constants/Colors';
+
+interface HeaderProps {
+  name: string;
+}
+
+export default function AdminHomeHeader({ name }: HeaderProps) {
+  return (
+    <SafeAreaView>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>
+          Back to the Grind,{' '}
+          <Text style={{ fontFamily: Fonts.semiboldItalic }}>{name}!</Text>
+        </Text>
+
+        <View style={styles.headerIcon}>
+            <FontAwesome name='bell-o' color={'black'} size={24} onPress={() => router.push('/(admin)/notifications')} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    width: '85%',
+    backgroundColor: Colors.bg,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  headerText: {
+    fontSize: 20,
+    fontFamily: Fonts.semibold,
+  },
+  headerIcon: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+  },
+});
