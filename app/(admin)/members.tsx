f2411cc61d045f97bd968be7760a7757639a6523
Jilliane Ferolino <158802020+jilyanf@users.noreply.github.com>
2025-04-11 11:22:48 +0800

    initial members screen layout

--- app/(admin)/members.tsx
+++ app/(admin)/members.tsx
@@ -1,12 +1,30 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
+import { useState } from 'react';
+import { AntDesign } from '@expo/vector-icons';
 
-import Header from '@/components/HistoryHeader';
+import Header from '@/components/AdminSectionHeaders';
 import { Colors } from '@/constants/Colors';
+import { Fonts } from '@/constants/Fonts';
 
 export default function HistoryScreen() {
+  const [searchQuery, setSearchQuery] = useState('');
+
   return (
     <View style={styles.container}>
-      <Header />
+      <Header screen='Gym Members' />
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Search"
+            placeholderTextColor={Colors.white}
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+          <AntDesign name="search1" size={20} color={Colors.white} style={styles.searchIcon} />
+            </View>
+        </View>
     </View>
   );
 }
@@ -16,5 +34,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.bg,
     alignItems: 'center',
+  },
+  searchContainer: {
+    width: '85%',
+    marginBottom: 10, 
+  },
+  searchWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: Colors.black,
+    borderRadius: 30,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  searchInput: {
+    flex: 1,
+    color: Colors.white,
+    fontFamily: Fonts.regular,
+  },
+  searchIcon: {
+    marginLeft: 10, 
   },
 });
