792499e750e765d9d05f03a77b170e4fb1a5529b
Alex <zcfigueroa@up.edu.ph>
2025-05-20 13:21:00 +0800

    new Storage refactor

---
+++ utils/storageUtils.ts
@@ -0,0 +1,53 @@
+import { Platform } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const isWeb = Platform.OS === 'web';
+
+// Save data
+const saveItem = async (key: string, value: string): Promise<void> => {
+  try {
+    if (isWeb) {
+        await saveItem(key, value);
+    } else {
+        SecureStore.setItemAsync(key, value, {
+            keychainAccessible: SecureStore.ALWAYS,
+        });
+    }
+  } catch (error) {
+        console.error('Save Error:', error);
+  }
+};
+
+// Get data
+const getItem = async (key: string): Promise<string | null> => {
+  try {
+    if (isWeb) {
+      return await getItem(key);
+    } else {
+      return await SecureStore.getItemAsync(key, {
+            keychainAccessible: SecureStore.ALWAYS,
+        });
+    }
+  } catch (error) {
+    console.error('Get Error:', error);
+    return null;
+  }
+};
+
+// Delete data
+const deleteItem = async (key: string): Promise<void> => {
+  try {
+    if (isWeb) {
+      await AsyncStorage.removeItem(key);
+    } else {
+      await SecureStore.deleteItemAsync(key, {
+            keychainAccessible: SecureStore.ALWAYS,
+        });;
+    }
+  } catch (error) {
+    console.error('Delete Error:', error);
+  }
+};
+
+export { saveItem, getItem, deleteItem };
